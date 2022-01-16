babc0d1074d706cefe892d230b6409c8d195ca36
ddld93 <umar.jere@gmail.com>
2022-01-16 18:42:11 +0100

    yes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,29 @@ import React from 'react'
 import "./navbar.css"
 
 function Navbar() {
+    const toggle = (e) => {
+        let mobileViews = document.querySelector(".mobile-view")
+        let body = document.querySelector(".body")
+        let nav = document.querySelector("nav")
+        body.classList.add("nav-toggle")
+        nav.classList.add("nav-toggle")
+        mobileViews.classList.add("toggle")
+        console.log(e.target)
+
+       
+    }
     return (
+        <div>
         <nav>
             <header className="header">
                 <img src="" alt="logo" />
                 <h3 className="title">Digital Reasoning</h3>
             </header>
-            <ul style={{display:"flex", alignItems:"center",gap:"20px"}} >
-                <li style={{visibility:"hidden"}} className="contact"><a href="#">Contact Us</a></li>
+            <ul id='list' className='small-navbar' >
+                <li className="contact"><a className='button' href="#">Contact Us</a></li>
                 <li><a href="#">Home</a></li>
-                <li style={{display:"flex", alignItems:"center"}} >
-                <a style={{display:"flex", alignItems:"center"}} href="#"> 
+                <li className='list' >
+                <a className='list' href="#"> 
                     
                 <svg width="25" height="25" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect width="33" height="33" fill="#E5E5E5"/>
@@ -25,13 +37,20 @@ function Navbar() {
                     Services</a></li>
                 <li><a href="#">About</a></li>
             </ul>
-            <div className="burger">
+            <div onClick={toggle} className="burger">
                 <hr />
                 <hr style={{borderBlockColor:"var(--primary-color)",width:"20px"}} />
                 <hr />
             </div>
-
         </nav>
+        <div className="mobile-view">
+                <ul>
+                    <li><a href="http://" target="_blank" rel="noopener noreferrer">Home</a></li>
+                    <li><a href="http://" target="_blank" rel="noopener noreferrer">Services</a></li>
+                    <li><a href="http://" target="_blank" rel="noopener noreferrer">About</a></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,23 +1,29 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import "./LandingPage.css";
-
+import Navbar from "../components/Navbar";
 function LandingPage() {
   return (
     <div>
       <Navbar />
-      <section className="section-one">
-        <h1>Digital</h1>
-        <h2>Reasoning</h2>
-        <p>
-          Digital Reasoning is a multidisciplinary corporate communication,public relations,perceptionmanagement,andresearchanddocumentationfirm. 
-          Weare built with a global vision and tailored to specific contemporary needs of our clients.
-        </p>
-        <button>Contact Us</button>
-      </section>
-      <section className="section-two">
+      <div className="body">
+        <div className="banner-wrapper">
+          <section className="section-one">
+            <h1>
+              Digital <span>Reasoning</span>
+            </h1>
 
-      </section>
+            <p>
+              Digital Reasoning is a multidisciplinary corporate
+              communication,public relations,perceptionmanagement,and research
+              and documentation firm. <br />
+              We are built with a global vision and tailored to specific
+              contemporary needs of our clients.
+            </p>
+            <button className="button">Contact Us</button>
+          </section>
+        </div>
+        <section className="section-two"></section>
+      </div>
     </div>
   );
 }
